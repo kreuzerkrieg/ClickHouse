d8e0b55b1684204bc9b3be1df0e945524534ac66
kreuzerkrieg <kreuzerkrieg@gmail.com>
2020-09-27 19:22:24 +0300

    add gperftools tcmalloc

--- base/common/memory.h
+++ base/common/memory.h
@@ -11,14 +11,14 @@
 #    include <cstdlib>
 #endif
 
-#ifdef USE_TCMALLOC_CPP
+#if USE_TCMALLOC_CPP
 #    include <tcmalloc/tcmalloc.h>
 #endif
 
 namespace Memory
 {
 
-#ifndef USE_TCMALLOC_CPP
+//#ifndef USE_TCMALLOC_CPP
 
 inline ALWAYS_INLINE void * newImpl(std::size_t size)
 {
@@ -39,29 +39,29 @@ inline ALWAYS_INLINE void deleteImpl(void * ptr) noexcept
 {
     free(ptr);
 }
-#else
-
-inline ALWAYS_INLINE void * newImpl(std::size_t size)
-{
-    auto * ptr = TCMallocInternalNew(size);
-    if (likely(ptr != nullptr))
-        return ptr;
-
-    /// @note no std::get_new_handler logic implemented
-    throw std::bad_alloc{};
-}
-
-inline ALWAYS_INLINE void * newNoExept(std::size_t size) noexcept
-{
-    return TCMallocInternalNewNothrow(size, std::nothrow_t());
-}
-
-inline ALWAYS_INLINE void deleteImpl(void * ptr) noexcept
-{
-    TCMallocInternalDelete(ptr);
-}
-
-#endif
+//#else
+//
+//inline ALWAYS_INLINE void * newImpl(std::size_t size)
+//{
+//    auto * ptr = TCMallocInternalNew(size);
+//    if (likely(ptr != nullptr))
+//        return ptr;
+//
+//    /// @note no std::get_new_handler logic implemented
+//    throw std::bad_alloc{};
+//}
+//
+//inline ALWAYS_INLINE void * newNoExept(std::size_t size) noexcept
+//{
+//    return TCMallocInternalNewNothrow(size, std::nothrow_t());
+//}
+//
+//inline ALWAYS_INLINE void deleteImpl(void * ptr) noexcept
+//{
+//    TCMallocInternalDelete(ptr);
+//}
+//
+//#endif
 
 #if USE_JEMALLOC && JEMALLOC_VERSION_MAJOR >= 4
 
